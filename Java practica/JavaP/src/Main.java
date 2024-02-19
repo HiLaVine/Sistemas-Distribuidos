2793496929f9386e8bf67c5f704e418da8e93cc9
HiLaVine <sinnafa01@gmail.com>
2024-02-18 20:13:35 -0600

    tarea dos

--- Java practica/JavaP/src/Main.java
+++ Java practica/JavaP/src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        Scanner scanner = new Scanner(System.in);
+
+        var usuario = scanner.nextLine();
+        System.out.println(usuario + " es tu nombre");
     }
 }
