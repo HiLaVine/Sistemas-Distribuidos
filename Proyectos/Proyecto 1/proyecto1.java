4256960293a2d4cfc43306d5ed104227d32b1c51
HiLaVine <sinnafa01@gmail.com>
2024-02-25 14:37:54 -0600

    Proyecto 1 terminado

--- Proyectos/Proyecto 1/proyecto1.java
+++ Proyectos/Proyecto 1/proyecto1.java
@@ -1,22 +1,47 @@
-import java.util.Scanner;
+/* 
+24 Leer un archivo de texto que contiene un número entero por cada línea e indicar cuál es el
+número mayor dentro del archivo y el número de renglón en que se encuentra.
+*/
 
-public class proyecto1{
-    public static void main(String [] args){
-
-        Scanner scanner = new Scanner(System.in);
-
-        double radio = scanner.nextDouble();
-        double altura = scanner.nextDouble();
-
-
-        double volumne_cono = radio * altura;
-
-        System.out.println("altura = " + altura);
-        System.out.println("radio = " + radio);
-        System.out.println("multi = %.4", volumne_cono);
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 
+public class proyecto1 {
+    public static void main(String[] args) {
+        File archivo = new File("numeros.txt");
 
+        int numeroMayor = Integer.MIN_VALUE; 
+        int numeroRenglon = -1;
+        int renglonActual = 0;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
+            String linea;
+
+            while ((linea = br.readLine()) != null) {
+                renglonActual++;
+
+                
+                int numero = Integer.parseInt(linea);
+
+                
+                if (numero > numeroMayor) {
+                    numeroMayor = numero;
+                    numeroRenglon = renglonActual;
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error al leer el archivo: " + e.getMessage());
+        } catch (NumberFormatException e) {
+            System.out.println("El archivo contiene datos no numéricos.");
+        }
+
+        if (numeroRenglon != -1) {
+            System.out.println("El número mayor es " + numeroMayor + " y se encuentra en el renglón " + numeroRenglon + ".");
+        } else {
+            System.out.println("El archivo está vacío o no contiene números.");
+        }
     }
-
 }
